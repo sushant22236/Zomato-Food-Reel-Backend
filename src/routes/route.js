02efe105291f68f7bfe7c039d53c5e7c439ed2af
Sushant Singh <sushant22007@gmail.com>
2025-09-17 20:22:37 +0530

    Add food partner routes for registration, login, and logout

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { login, logout, register } from '../controllers/auth.controller.js';
+import { registerFoodPartner, loginFoodPartner, logoutFoodPartner } from '../controllers/foodPartner.controller.js';
 
 const router = express.Router();
 
@@ -7,6 +8,8 @@ router.post('/register', register)
 router.post('/login', login)
 router.post('/logout', logout)
 
-router.post('/')
+router.post('/food-partner-register', registerFoodPartner)
+router.post('/food-partner-login', loginFoodPartner)
+router.post('/food-partner-logout', logoutFoodPartner)
 
 export default router;
