32356522f7ff3a0433b86acfebee59fdece183ce
Sushant Singh <sushant22007@gmail.com>
2025-09-19 16:37:41 +0530

    Add CORS support to the application

--- src/app.js
+++ src/app.js
@@ -3,8 +3,14 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db.connection.js";
 import apiroute from "./routes/route.js";
 import foodroute from "./routes/food.route.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors({
+  origin: "*", // or replace "*" with your frontend cloudflared URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 connectDB();
 
